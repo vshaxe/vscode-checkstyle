adc172fa94291867b8e0425bf52ab56468045763
Valentin Lemière <me@ibilon.com>
2016-05-03 18:33:35 +0200

    Add option to use checkstyle configuration file

--- src/Main.hx
+++ src/Main.hx
@@ -1,8 +1,11 @@
+import haxe.io.Path;
 import vscode.ExtensionContext;
 import vscode.TextDocument;
 import vscode.DiagnosticCollection;
 
 class Main {
+    static inline var CONFIG_OPTION = "configurationFile";
+
     var context:ExtensionContext;
     var diagnostics:DiagnosticCollection;
 
@@ -19,10 +22,28 @@ class Main {
             return;
         }
         var checker = new checkstyle.Main();
-        checker.addAllChecks();
+
+        var configuration = Vscode.workspace.getConfiguration("haxecheckstyle");
+        if (configuration.has(CONFIG_OPTION) && configuration.get(CONFIG_OPTION) != "") {
+            try {
+                checker.configPath = Path.join([Vscode.workspace.rootPath, configuration.get(CONFIG_OPTION)]);
+                trace(checker.configPath);
+                checker.loadConfig(checker.configPath);
+            }
+            catch (e:Dynamic) {
+                trace(e);
+                checker.configPath = null;
+            }
+        }
+
+        if (checker.configPath == null) {
+            checker.addAllChecks();
+        }
+
         var file:Array<checkstyle.CheckFile> = [{ name: fileName, content: null, index: 0 }];
         var reporter = new VSCodeReporter(1, checker.getCheckCount(), checker.checker.checks.length, null, false);
         checker.checker.addReporter(reporter);
+
         checker.checker.process(file, checker.excludesMap);
         diagnostics.set(vscode.Uri.file(fileName), reporter.diagnostics);
     }
