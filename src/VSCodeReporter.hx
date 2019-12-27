f9118ed8345ec37e699ec40a9e8668bf4fcd50fa
AlexHaxe <Alexander.Blum@gmail.com>
2019-12-27 18:26:28 +0100

    avoid filling VSCode's debug console with summary messages

--- src/VSCodeReporter.hx
+++ src/VSCodeReporter.hx
@@ -6,6 +6,10 @@ import vscode.Range;
 class VSCodeReporter extends BaseReporter {
 	public var diagnostics:Array<Diagnostic> = [];
 
+	override public function start() {}
+
+	override public function finish() {}
+
 	override public function addMessage(m:CheckMessage) {
 		var range = new Range(m.startLine - 1, m.startColumn, m.endLine - 1, m.endColumn);
 		var diag = new Diagnostic(range, m.moduleName + " - " + m.message, Information);
